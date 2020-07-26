53714ba0b65fcfce006aeed677c23e1f2c9c42b7
Daniel <KasumiL5x@gmail.com>
2020-07-26 10:01:05 +0100

    Again, PS complaining

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -9,7 +9,9 @@ public class ItemController : Controller {
   private readonly ApplicationDbContext _context;
 
   public ItemController(ApplicationDbContext context) {
-    this._context = context;
+    // PS complaining again.
+    _context = context;
+    // this._context = context;
   }
   
   public IActionResult Index() {
@@ -29,21 +31,22 @@ public class ItemController : Controller {
   }
 
   public IActionResult Delete(int id) {
-
-    // Making PS happy...
     var item = _context.Items.FirstOrDefault(e => e.Id == id);
     _context.Items.Remove(item);
     _context.SaveChanges();
     return RedirectToAction("Index");
-
-    // PluralSight doesn't like this even though it's right.
-    // var item = _context.Items.First(x => x.Id == id);
-    // if(item != null) {
-    //   _context.Items.Remove(item);
-    //   _context.SaveChanges();
-    // }
-    // return RedirectToAction("Index");
   }
+
+  // PluralSight doesn't like this even though it's right.
+  // public IActionResult Delete(int id) {
+  //   var item = _context.Items.First(x => x.Id == id);
+  //   if(item != null) {
+  //     _context.Items.Remove(item);
+  //     _context.SaveChanges();
+  //   }
+  //   return RedirectToAction("Index");
+  // }
+
 }
 
 }
