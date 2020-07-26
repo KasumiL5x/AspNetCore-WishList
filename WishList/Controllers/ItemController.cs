52b5202178b7def2c6d6ed666d622021e033b518
Daniel <KasumiL5x@gmail.com>
2020-07-26 09:57:18 +0100

    Final task (controllers).

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers {
+
+public class ItemController : Controller {
+  private readonly ApplicationDbContext _context;
+
+  public ItemController(ApplicationDbContext context) {
+    this._context = context;
+  }
+  
+  public IActionResult Index() {
+    return View("Index", _context.Items.ToList());
+  }
+
+  [HttpGet]
+  public IActionResult Create() {
+    return View("Create");
+  }
+
+  [HttpPost]
+  public IActionResult Create(Item item) {
+    _context.Items.Add(item);
+    _context.SaveChanges();
+    return RedirectToAction("Index");
+  }
+
+  public IActionResult Delete(int id) {
+    var item = _context.Items.First(x => x.Id == id);
+    if(item != null) {
+      _context.Items.Remove(item);
+      _context.SaveChanges();
+    }
+    return RedirectToAction("Index");
+  }
+}
+
+}
